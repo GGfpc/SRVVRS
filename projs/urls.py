bef986972836d2754e971070a2ea4f650921b35a
GGfpc <ggfpc12495@gmail.com>
2016-04-24 19:48:37 +0100

    Votos funcionam

--- projs/urls.py
+++ projs/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [
-    url(r'^$',views.lista_de_projs,name='lista_de_projectos')
 
+app_name = 'projs'
+
+urlpatterns = [
+    url(r'^$', views.lista_de_projs,name='lista_de_projectos'),
+    url(r'^(?P<post_id>[0-9]+)/voto/$', views.voto, name='voto'),
 ]
--- projs/views.py
+++ projs/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .models import Projecto
 from time import timezone
+from django.http import HttpResponseRedirect, HttpResponse
 
 def lista_de_projs(request):
     post = Projecto.objects.order_by('-votos')
     return render(request,'projs/lista_de_projs.html',{'post' : post})
+
+def voto(request, post_id):
+    post = Projecto.objects.get(id=post_id)
+    post.votos += 1
+    post.save()
+    return HttpResponseRedirect("/")
+
 
