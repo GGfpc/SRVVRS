299c6618e981c37c77e5bdf26352f29ef7c0f472
GGfpc <ggfpc12495@gmail.com>
2016-04-24 15:45:11 +0100

    Adicionei models empregado e projecto

--- projs/models.py
+++ projs/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.utils import timezone
 
-class Projecto(models.Model):
-    titulo = models.CharField
 
+class Empregado(models.Model):
+    nome = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome + " - " + self.titulo
+
+class Projecto(models.Model):
+    autor = models.ForeignKey(Empregado,on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=500)
+    texto = models.TextField
+    data = models.DateTimeField(default=timezone.now)
+
+    def publicaProj(self):
+        self.data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
 
