858d18ae8c33a75a7daea19eed6e37112da1102a
GGfpc <ggfpc12495@gmail.com>
2016-05-04 19:07:27 +0100

    login feito

--- projs/urls.py
+++ projs/urls.py
@@ -7,5 +7,6 @@ app_name = 'projs'
 urlpatterns = [
     url(r'^$', views.lista_de_projs,name='lista_de_projectos'),
     url(r'^(?P<post_id>[0-9]+)/voto/$', views.voto, name='voto'),
-    url(r'^home$', views.home, name='home')
+    url(r'^home$', views.home, name='home'),
+    url(r'^logging',views.logging, name='logging'),
 ]
